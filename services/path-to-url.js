c876f3cc2d51b44b55da2ec65b95bc225b9a5fc1
laoshu133 <admin@laoshu133.com>
2017-05-09 15:40:20 +0800

    Fix path-to-url

--- services/path-to-url.js
+++ services/path-to-url.js
@@ -8,8 +8,12 @@ const lodash = require('lodash');
 
 module.exports = function(p) {
     const env = process.env;
-    const urlPrefix = lodash.trim(env.OUT_PATH_URL_PREFIX, '/');
-    const uri = path.join('/', urlPrefix, p);
+    const OUT_PATH = env.OUT_PATH;
+    const OUT_PATH_URL_PREFIX = env.OUT_PATH_URL_PREFIX;
+
+    const urlPrefix = lodash.trim(OUT_PATH_URL_PREFIX, '/');
+    const innerPath = path.relative(OUT_PATH, p);
+    const uri = path.join('/', urlPrefix, innerPath);
 
     return `${env.WWW_PROTOCOL}://${env.WWW_HOST}${uri}`;
 };
