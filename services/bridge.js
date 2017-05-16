b11764a63889b1283ff30b16b5aa3e8330dbe7c7
laoshu133 <admin@laoshu133.com>
2017-05-17 00:59:30 +0800

    Reflect bridge

--- services/bridge.js
+++ services/bridge.js
@@ -2,6 +2,16 @@
  * bridge
  */
 
-const Bridge = require('../lib/bridge');
+const BridgeBase = require('../lib/bridge');
+
+const CDP_CLIENT_MAX_COUNT = +env.CDP_CLIENT_MAX_COUNT || 10;
+
+class Bridge extends BridgeBase {
+    constructor(...args) {
+        super(...args);
+
+        this.clients = [];
+    }
+}
 
 module.exports = new Bridge(process.env.CDP_HOST);
