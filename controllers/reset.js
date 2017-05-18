a6c61d029386914f7eb849428168b036dbf0eb96
laoshu133 <admin@laoshu133.com>
2017-05-18 11:56:17 +0800

    Reflect bridge

--- controllers/reset.js
+++ controllers/reset.js
@@ -19,6 +19,8 @@ module.exports = function(router) {
             return bridge.closeClientById(target.id);
         });
 
+        yield bridge.syncClients();
+
         this.body = targets;
     });
 };
