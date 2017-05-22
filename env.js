59983d6e90e487c7a2be904044ef16741c6934df
xiaomi <xiaomi@huanleguang.com>
2017-05-22 11:40:27 +0800

    Update env config

--- env.js
+++ env.js
@@ -2,9 +2,16 @@
  * global env
  */
 
+const fs = require('fs');
+
+const userEnvFile = `${__dirname}/.env`;
+const globalEnvFile = `${__dirname}/.env.example`;
+
+const envFilePath = fs.existsSync(userEnvFile) ? userEnvFile : globalEnvFile;
+
 // global env
 require('dotenv-safe').load({
     allowEmptyValues: true,
-    path: `${__dirname}/.env`,
-    sample: `${__dirname}/.env.example`
+    sample: globalEnvFile,
+    path: envFilePath
 });
