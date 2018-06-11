8ea575cb09fad61ee9c6c72abb67d6d02f1ffc66
xiaomi <xiaomi@huanleguang.com>
2018-06-11 18:39:45 +0800

    chore: Update logger dep

--- services/logger.js
+++ services/logger.js
@@ -5,37 +5,21 @@
  */
 
 const debug = require('debug');
+const Logger = require('hlg-logger');
 
 const APP_NAME = process.env.DEBUG.replace(/:\*$/, '');
 
-const logger = require('hlg-logger').create({
+const logger = Logger.create({
     logServer: process.env.LOG_SERVER,
     app: APP_NAME
 });
 
-
 // debug.formatArgs
 // 非 TTY 输出时，格式化时间为当地时间
 if(!process.stdout.isTTY) {
-    // let rSingleNum = /\b(\d)\b/g;
-
     debug.formatArgs = function() {
-        let args = arguments;
-        let name = this.namespace;
-
-        // let now = new Date();
-        // let timeStr = [
-        //     now.getFullYear() + '/',
-        //     now.getMonth() + 1 + '/',
-        //     now.getDate() + ' ',
-        //     now.getHours() + ':',
-        //     now.getMinutes() + ':',
-        //     now.getSeconds()
-        // ]
-        // .join('')
-        // .replace(rSingleNum, '0$1');
-
-        // args[0] = timeStr +' '+ name +' '+ args[0];
+        const args = arguments;
+        const name = this.namespace;
 
         // 取消内部格式化时间，使用 PM2 时间戳
         args[0] = name + ' ' + args[0];
