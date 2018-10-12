929bfbdbe78b6b0dfc30481b7772bb79df1508af
xiaomi <xiaomi@huanleguang.com>
2018-10-12 17:53:55 +0800

    chor: Update id creater

--- services/parse-config.js
+++ services/parse-config.js
@@ -36,7 +36,7 @@ const getLocalConfigPromise = Promise.try(() => {
 // Switch dir per 10 mins
 const getCurrOutPath = (id = 'tmp') => {
     const now = Date.now();
-    const interval = 30 * 60 * 1000;
+    const interval = 20 * 60 * 1000;
     const prefixDir = String(Math.floor(now / interval));
 
     let outPath = process.env.OUT_PATH;
