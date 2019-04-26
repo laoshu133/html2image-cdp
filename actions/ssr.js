1195b92145c632d6b6cd47342b4ba2d4587ec4da
xiaomi <xiaomi@huanleguang.com>
2019-04-26 16:40:10 +0800

    fix: fix ssr sometimes crash in XMLSerializer

--- actions/ssr.js
+++ actions/ssr.js
@@ -7,7 +7,19 @@ const BaseAction = require('./base');
 class SSR extends BaseAction {
     async main() {
         const page = this.page;
-        const html = await page.content();
+        const html = await page.evaluate(() => {
+            let ret = document.doctype
+                // fix ssr sometimes crash in XMLSerializer
+                // ? new XMLSerializer().serializeToString(document.doctype)
+                ? '<!DOCTYPE html>'
+                : '';
+
+            if(document.documentElement) {
+                ret += document.documentElement.outerHTML;
+            }
+
+            return ret;
+        });
 
         this.result = {
             metadata: {
